fdc2b740ca5a267abf0e8d78c02d6338920c453e
patataofcourse <patataofcourse@gmail.com>
2022-04-08 17:26:14 +0200

    BRCAD: Sprites!

--- src/bxcad/brcad.rs
+++ src/bxcad/brcad.rs
@@ -40,7 +40,6 @@ pub struct SpritePart {
     pub flip_x: bool,
     pub flip_y: bool,
     pub opacity: u8,
-    pub unk1: u8, //terminator?
 }
 
 #[derive(Serialize, Deserialize)]
@@ -66,9 +65,59 @@ impl BXCAD<'_> for BRCAD {
     const TIMESTAMP: u32 = 20100312;
     const BXCAD_TYPE: BXCADType = BXCADType::BRCAD;
     fn from_binary<F: Read + Seek>(f: &mut F) -> IOResult<Self> {
+        let timestamp = u32::read_from(f, Self::BYTE_ORDER)?;
+        let unk0 = u32::read_from(f, Self::BYTE_ORDER)?;
+        let spritesheet_num = u16::read_from(f, Self::BYTE_ORDER)?;
+        let spritesheet_control = u16::read_from(f, Self::BYTE_ORDER)?;
+        let texture_width = u16::read_from(f, Self::BYTE_ORDER)?;
+        let texture_height = u16::read_from(f, Self::BYTE_ORDER)?;
+
+        let sprite_count = u32::read_from(f, Self::BYTE_ORDER)?;
+        let mut sprites = vec![];
+        for _ in 0..sprite_count {
+            let parts_count = u32::read_from(f, Self::BYTE_ORDER)?;
+            let mut parts = vec![];
+            for _ in 0..parts_count {
+                let texture_pos = PosInTexture {
+                    x: u16::read_from(f, Self::BYTE_ORDER)?,
+                    y: u16::read_from(f, Self::BYTE_ORDER)?,
+                    width: u16::read_from(f, Self::BYTE_ORDER)?,
+                    height: u16::read_from(f, Self::BYTE_ORDER)?,
+                };
+                let unk0 = u32::read_from(f, Self::BYTE_ORDER)?;
+                let pos_x = u16::read_from(f, Self::BYTE_ORDER)?;
+                let pos_y = u16::read_from(f, Self::BYTE_ORDER)?;
+                let scale_x = f32::read_from(f, Self::BYTE_ORDER)?;
+                let scale_y = f32::read_from(f, Self::BYTE_ORDER)?;
+                let rotation = f32::read_from(f, Self::BYTE_ORDER)?;
+                let flip_x = bool::read_from(f, Self::BYTE_ORDER)?;
+                let flip_y = bool::read_from(f, Self::BYTE_ORDER)?;
+                let opacity = u8::read_from(f, Self::BYTE_ORDER)?;
+                u8::read_from(f, Self::BYTE_ORDER)?; // terminator/padding
+                parts.push(SpritePart {
+                    texture_pos,
+                    unk0,
+                    pos_x,
+                    pos_y,
+                    scale_x,
+                    scale_y,
+                    rotation,
+                    flip_x,
+                    flip_y,
+                    opacity,
+                })
+            }
+            sprites.push(Sprite { parts });
+        }
         unimplemented!();
     }
     fn to_binary<F: Write>(&self, f: &mut F) -> IOResult<()> {
         unimplemented!();
+    }
+}
+
+impl BRCAD {
+    pub fn apply_labels<F: Read>(&self, labels: &mut F) -> IOResult<()> {
+        unimplemented!()
     }
 }
